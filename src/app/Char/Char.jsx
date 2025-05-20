98caf8703c3d8d9e026fa76c6c0a3b246f475610
abuyusufrony <abuyusufrony@gmail.com>
2025-05-21 01:14:19 +0600

    dynamic-meta-route

--- src/app/Char/Char.jsx
+++ src/app/Char/Char.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Char = () => {
+import Link from 'next/link';
+import allchar from '../../../lib/Allchar';
+
+
+async function Char() {
+    const rick = await allchar()
+
+
+
     return (
-        <div>
-            <h2>this charjsx</h2>
+        <div className='bg-[#0B0C2A] border border-[#00FFF0]    mr-4'>
+            this page are  characters.
+
+            <div className='text-white flex  justify-between'>
+                <div>
+                    <h2>Meet the cast</h2>
+                </div>
+                <button>View All</button>
+            </div>
+            <div>
+                {
+                    rick.map((r) => <div> <Link href={`/Char/${r.id}`}>
+                        <img src={r.image} alt={r.name} />
+
+                    </Link> </div>)
+                }
+            </div>
+
         </div>
     );
 };
--- src/app/Char/[id]/page.jsx
+++ src/app/Char/[id]/page.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import SingleChar from '../../../../lib/Singlechar';
 
-const page = () => {
+export async function generateMetadata({ params }) {
+    const { id } = params;
+    const schar = await SingleChar(id)
+
+    return {
+        title: schar.name,
+        description: schar.species
+    };
+
+}
+
+export default async function page({ params }) {
+    const { id } = params
+    const schar = await SingleChar(id)
+
     return (
         <div>
             <h2>dynamic route</h2>
+            <p>id are {id}</p>
+            <h2> name are :{schar.species}</h2>
+            <img src={schar.image} alt="" />
+
         </div>
     );
 };
 
-export default page;
